90a9a52f9ead8c07179e925c23af74ed0bb33e6c
Sewef <27089398+Sewef@users.noreply.github.com>
2025-12-08 10:51:49 +0100

    Delete GoogleSheetsConfig.js

--- src/commands/integrations/GoogleSheetsConfig.js
+++
@@ -1,2 +0,0 @@
-// Backwards-compatible re-exports from GoogleSheets.js
-export { getGoogleSheetsCredentials, saveGoogleSheetsApiKey, saveGoogleSheetsSheetId, hasGoogleSheetsCredentials, GSHEET_API_KEY_STORAGE, GSHEET_SHEET_ID_STORAGE } from './GoogleSheets.js';
